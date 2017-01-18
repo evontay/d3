0da583e00be7a65c5d2c85b1a90d6d6d45609257
Tay <fumblies@gmail.com>
2017-01-18 17:35:15 +0800

    creating svg elements based on data

--- script.js
+++ script.js
@@ -44,3 +44,30 @@ var someData = [1,2,3]
     .text(function (d,i) {
       return "index = " + i + " d = " + d;
     });
+
+// Creating SVG data circle visualisation
+var circleRadii = [40, 20, 10]
+
+var svgContainer = d3.select('body').append('svg')
+  .attr('width', 200)
+  .attr('height', 200)
+  .attr('class', "red");
+
+var circles = svgContainer.selectAll('circle')
+                          .data(circleRadii)
+                          .enter()
+                          .append('circle');
+
+var circleAttributes = circles
+                        .attr('cx', 50)
+                        .attr('cy', 50)
+                        .attr('r', function (d){
+                          return d;
+                        })
+                        .attr('fill', function (d) {
+                          var returnColor;
+                          if (d === 40) {returnColor = "blueviolet";
+                          } else if (d === 20) {returnColor = "blue";
+                          } else if (d === 10) {returnColor = "darkblue";}
+                          return returnColor;
+                        });
